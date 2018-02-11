e7bd64959f307c01654cc36313b55784ff6f4ab5
stufforama <stufforama.arh@gmail.com>
2018-02-11 23:19:04 +0300

    test

--- bot.py
+++ bot.py
@@ -67,111 +67,115 @@ start_msg = 'Чем я могу помочь?'
 # global last_message
 last_message = ''
 
-def nearest_service(location):
-    min_dist = 10000000
-    nearest_sc = ''
-    sc_description = ''
-    for index in range(services.shape[0]):
-        distance = np.sqrt((location.latitude-services.lat[index])**2 + (location.longitude-services.lng[index])**2)
-        if distance < min_dist:
-            min_dist = distance
-            nearest_sc = index
-    sc_longitude = services['lng'].loc[nearest_sc]
-    sc_latitude = services['lat'].loc[nearest_sc]
-    sc_description = '\n'.join(list(services[useful_cols].loc[nearest_sc]))
-    return sc_longitude, sc_latitude, sc_description    
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-def manual_nearest_service(lat, lng):
-    min_dist = 10000000
-    nearest_sc = ''
-    sc_description = ''
-    for index in range(services.shape[0]):
-        distance = np.sqrt((lat-services.lat[index])**2 + (lng-services.lng[index])**2)
-        if distance < min_dist:
-            min_dist = distance
-            nearest_sc = index
-    sc_longitude = services['lng'].loc[nearest_sc]
-    sc_latitude = services['lat'].loc[nearest_sc]
-    sc_description = '\n'.join(list(services[useful_cols].loc[nearest_sc]))
-    return sc_longitude, sc_latitude, sc_description  
+# def nearest_service(location):
+#     min_dist = 10000000
+#     nearest_sc = ''
+#     sc_description = ''
+#     for index in range(services.shape[0]):
+#         distance = np.sqrt((location.latitude-services.lat[index])**2 + (location.longitude-services.lng[index])**2)
+#         if distance < min_dist:
+#             min_dist = distance
+#             nearest_sc = index
+#     sc_longitude = services['lng'].loc[nearest_sc]
+#     sc_latitude = services['lat'].loc[nearest_sc]
+#     sc_description = '\n'.join(list(services[useful_cols].loc[nearest_sc]))
+#     return sc_longitude, sc_latitude, sc_description    
 
-@bot.message_handler(commands=["start", 'menu'])
-def dial_start(message):
-    bot.send_message(message.chat.id, start_msg, reply_markup = keyboard_layout)
+# def manual_nearest_service(lat, lng):
+#     min_dist = 10000000
+#     nearest_sc = ''
+#     sc_description = ''
+#     for index in range(services.shape[0]):
+#         distance = np.sqrt((lat-services.lat[index])**2 + (lng-services.lng[index])**2)
+#         if distance < min_dist:
+#             min_dist = distance
+#             nearest_sc = index
+#     sc_longitude = services['lng'].loc[nearest_sc]
+#     sc_latitude = services['lat'].loc[nearest_sc]
+#     sc_description = '\n'.join(list(services[useful_cols].loc[nearest_sc]))
+#     return sc_longitude, sc_latitude, sc_description  
+
+# @bot.message_handler(commands=["start", 'menu'])
+# def dial_start(message):
+#     bot.send_message(message.chat.id, start_msg, reply_markup = keyboard_layout)
     
 
-@bot.message_handler(content_types=['text', 'location'])
-def response(message):
-    global last_message
-    geo_request = 'Пожалуйста, укажите свой город и адрес'
-    sn_request = 'Введите серийный номер в формате [Модель][пробел][Серийный номер]'
-    check_sn = 'Проверьте правильность ввода'
-    manual_request = 'Выберите интересующую катеорию'
-    if message.text == 'Информация о поверке':
-        markup = telebot.types.ForceReply(selective=False)
-        bot.send_message(message.chat.id, sn_request, reply_markup=markup)
-        last_message = sn_request
-    elif message.text == 'Ближайший сервисный центр':
-        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-        button_geo = telebot.types.KeyboardButton(text="Определить автоматически", request_location=True)
-        markup.add(button_geo)
-        bot.send_message(message.chat.id, geo_request, reply_markup=markup)   
-        last_message = geo_request
-    elif message.text == 'Видеоинструкции':
-        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-        button_man_ton = telebot.types.KeyboardButton(text="Автоматический тонометр")
-        button_man_ing = telebot.types.KeyboardButton(text="Компрессорный ингалятор")
-        button_man_therm = telebot.types.KeyboardButton(text="Электронный термометр")
-        markup.add(button_man_ton)
-        markup.add(button_man_ing)
-        markup.add(button_man_therm)
-        bot.send_message(message.chat.id, manual_request, reply_markup=markup)   
-        last_message = manual_request
-    elif message.text in manuals:
-        bot.send_message(message.chat.id, manuals[message.text], reply_markup = keyboard_layout)
-        last_message = manuals[message.text]
-    elif message.reply_to_message != None:
-        if (message.reply_to_message.text == sn_request) | (message.reply_to_message.text == check_sn):
-            try:
-                sku = re.findall(string=message.text, pattern = '[0-9]{3,4}')[0]
-                if (sku == '911')&(len(re.findall(string=message.text, pattern = '[cC]+'))>0):
-                    sku = '911c'
+# @bot.message_handler(content_types=['text', 'location'])
+# def response(message):
+#     global last_message
+#     geo_request = 'Пожалуйста, укажите свой город и адрес'
+#     sn_request = 'Введите серийный номер в формате [Модель][пробел][Серийный номер]'
+#     check_sn = 'Проверьте правильность ввода'
+#     manual_request = 'Выберите интересующую катеорию'
+#     if message.text == 'Информация о поверке':
+#         markup = telebot.types.ForceReply(selective=False)
+#         bot.send_message(message.chat.id, sn_request, reply_markup=markup)
+#         last_message = sn_request
+#     elif message.text == 'Ближайший сервисный центр':
+#         markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+#         button_geo = telebot.types.KeyboardButton(text="Определить автоматически", request_location=True)
+#         markup.add(button_geo)
+#         bot.send_message(message.chat.id, geo_request, reply_markup=markup)   
+#         last_message = geo_request
+#     elif message.text == 'Видеоинструкции':
+#         markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+#         button_man_ton = telebot.types.KeyboardButton(text="Автоматический тонометр")
+#         button_man_ing = telebot.types.KeyboardButton(text="Компрессорный ингалятор")
+#         button_man_therm = telebot.types.KeyboardButton(text="Электронный термометр")
+#         markup.add(button_man_ton)
+#         markup.add(button_man_ing)
+#         markup.add(button_man_therm)
+#         bot.send_message(message.chat.id, manual_request, reply_markup=markup)   
+#         last_message = manual_request
+#     elif message.text in manuals:
+#         bot.send_message(message.chat.id, manuals[message.text], reply_markup = keyboard_layout)
+#         last_message = manuals[message.text]
+#     elif message.reply_to_message != None:
+#         if (message.reply_to_message.text == sn_request) | (message.reply_to_message.text == check_sn):
+#             try:
+#                 sku = re.findall(string=message.text, pattern = '[0-9]{3,4}')[0]
+#                 if (sku == '911')&(len(re.findall(string=message.text, pattern = '[cC]+'))>0):
+#                     sku = '911c'
                     
-                serian_number = int(''.join(re.findall(string=message.text, pattern = '[0-9]{4,}')[-2:]))
-                fltr = data.loc[(data['sku']==sku)&(data['start']<=serian_number)&(data['end']>=serian_number)]['url'].max()
-                if str(fltr) != 'nan':
-                    response = fltr
-                    bot.send_message(message.chat.id, response, reply_markup = keyboard_layout)
-                else:
-                    response = check_sn
-                    markup = telebot.types.ForceReply(selective=False)
-                    bot.send_message(message.chat.id, response, reply_markup = markup)
-            except IndexError:
-                response = check_sn
-                markup = telebot.types.ForceReply(selective=False)
-                bot.send_message(message.chat.id, response, reply_markup = markup)
-            last_message = response
-    elif message.location != None:
-        nearest_sc_longitude, nearest_sc_latitude, nearest_sc_descr = nearest_service(message.location)
-        bot.send_message(message.chat.id, nearest_sc_descr)
-        bot.send_location(message.chat.id, longitude=nearest_sc_longitude, latitude=nearest_sc_latitude,  reply_markup=keyboard_layout)
-        last_message = nearest_sc_descr       
-    elif (last_message == geo_request) & (message.location == None):
-        try:
-            manual_location = message.text
-            geocode = gmaps.geocode(manual_location)
-            manual_lat = geocode[0]['geometry']['location']['lat']
-            manual_lng = geocode[0]['geometry']['location']['lng']
-            nearest_sc_longitude, nearest_sc_latitude, nearest_sc_descr = manual_nearest_service(manual_lat, manual_lng)
-            bot.send_message(message.chat.id, nearest_sc_descr)
-            bot.send_location(message.chat.id, longitude=nearest_sc_longitude, latitude=nearest_sc_latitude,  reply_markup=keyboard_layout)
-            last_message = nearest_sc_descr
-        except IndexError:
-            markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-            button_geo = telebot.types.KeyboardButton(text="Определить автоматически", request_location=True)
-            markup.add(button_geo)
-            bot.send_message(message.chat.id, geo_request, reply_markup=markup)   
-            last_message = geo_request
+#                 serian_number = int(''.join(re.findall(string=message.text, pattern = '[0-9]{4,}')[-2:]))
+#                 fltr = data.loc[(data['sku']==sku)&(data['start']<=serian_number)&(data['end']>=serian_number)]['url'].max()
+#                 if str(fltr) != 'nan':
+#                     response = fltr
+#                     bot.send_message(message.chat.id, response, reply_markup = keyboard_layout)
+#                 else:
+#                     response = check_sn
+#                     markup = telebot.types.ForceReply(selective=False)
+#                     bot.send_message(message.chat.id, response, reply_markup = markup)
+#             except IndexError:
+#                 response = check_sn
+#                 markup = telebot.types.ForceReply(selective=False)
+#                 bot.send_message(message.chat.id, response, reply_markup = markup)
+#             last_message = response
+#     elif message.location != None:
+#         nearest_sc_longitude, nearest_sc_latitude, nearest_sc_descr = nearest_service(message.location)
+#         bot.send_message(message.chat.id, nearest_sc_descr)
+#         bot.send_location(message.chat.id, longitude=nearest_sc_longitude, latitude=nearest_sc_latitude,  reply_markup=keyboard_layout)
+#         last_message = nearest_sc_descr       
+#     elif (last_message == geo_request) & (message.location == None):
+#         try:
+#             manual_location = message.text
+#             geocode = gmaps.geocode(manual_location)
+#             manual_lat = geocode[0]['geometry']['location']['lat']
+#             manual_lng = geocode[0]['geometry']['location']['lng']
+#             nearest_sc_longitude, nearest_sc_latitude, nearest_sc_descr = manual_nearest_service(manual_lat, manual_lng)
+#             bot.send_message(message.chat.id, nearest_sc_descr)
+#             bot.send_location(message.chat.id, longitude=nearest_sc_longitude, latitude=nearest_sc_latitude,  reply_markup=keyboard_layout)
+#             last_message = nearest_sc_descr
+#         except IndexError:
+#             markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+#             button_geo = telebot.types.KeyboardButton(text="Определить автоматически", request_location=True)
+#             markup.add(button_geo)
+#             bot.send_message(message.chat.id, geo_request, reply_markup=markup)   
+#             last_message = geo_request
             
 
 @server.route('/' + token, methods=['POST'])
