d45c4f14ee8aec26c004be38f81dd11f674fa925
stufforama <stufforama.arh@gmail.com>
2018-02-11 17:51:58 +0300

    upd

--- bot.py
+++ bot.py
@@ -191,7 +191,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://aqueous-castle-59113.heroku.com/bot")
+    bot.set_webhook(url="https://infinite-waters-96978.heroku.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
