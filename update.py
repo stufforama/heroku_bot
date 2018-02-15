305f8d7b7217df37ed969ad842dad830edffdd64
stufforama <stufforama.arh@gmail.com>
2018-02-15 15:32:24 +0300

    fix 635

--- update.py
+++ update.py
@@ -19,7 +19,7 @@ for author in ['calibrpressuread', 'termometrcalibration']:
     for page in range(1,200):
         urls.append(url_template.format(author, page))
         
-flatten = lambda l: [item for sublist in l for item in sublist]
+# flatten = lambda l: [item for sublist in l for item in sublist]
 
 
 # In[ ]:
@@ -30,8 +30,8 @@ for url in urls:
         html = requests.get(url).text
         dom = fromstring(html)
         dom.make_links_absolute(url)
-        css_elements = flatten (dom.cssselect('.article'))
-        text += "\n".join([t.text_content().strip() +';' + t.get('href').strip() for t in css_elements])
+        css_elements = dom.cssselect('.menu-left')
+        text += "\n".join([css_elements[t].text_content().strip() +';' + css_elements[t].get('href').strip() for t in range(3,len(css_elements))])
         if css_elements != []:
             text += '\n'
     except AttributeError:
@@ -42,7 +42,7 @@ data = pd.read_csv(StringIO(text), sep=";",header=None, encoding = 'utf8')
 data.columns = ['text', 'url']
 pattern = '-[0-9]{3,4}'
 data['sku'] = data['text'].apply(lambda x: re.findall(string=x, pattern = pattern)[0].rsplit('-')[1])
-data.loc[data['sku']=='911', 'sku'] = data.loc[data['sku']=='911']['text'].apply(lambda x: '911c' if len(re.findall(string=x, pattern='[cC]+'))>0 else x)
+data.loc[data['sku']=='911', 'sku'] = data.loc[data['sku']=='911']['text'].apply(lambda x: '911c' if len(re.findall(string=x, pattern='[cC]+'))>0 else '911')
 data['start'] = data['text'].apply(lambda x: re.findall(string=x, pattern = '[0-9]{6,}')[0])
 data['end'] = data['text'].apply(lambda x: re.findall(string=x, pattern = '[0-9]{6,}')[1])
 data['start'] = pd.to_numeric(data['start'])
