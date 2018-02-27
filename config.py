c3e72b612dbecd31754343d6d7a3c066b7b9a522
stufforama <stufforama.arh@gmail.com>
2018-02-27 12:06:01 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-﻿manuals = {'Автоматический тонометр':'[Для просмотра щелкните на картинку](https://youtu.be/nTWcDJGSiwo) :arrow_down::arrow_down::arrow_down:', 
-           'Компрессорный ингалятор':'[Для просмотра щелкните на картинку](https://youtu.be/JY1urhMFnTg) :arrow_down::arrow_down::arrow_down:',
-           'Электронный термометр': '[Для просмотра щелкните на картинку](https://youtu.be/iA6yIxAYawE) :arrow_down::arrow_down::arrow_down:'}
+﻿manuals = {'Автоматический тонометр':'[Для просмотра щелкните на картинку](https://youtu.be/nTWcDJGSiwo)', 
+           'Компрессорный ингалятор':'[Для просмотра щелкните на картинку](https://youtu.be/JY1urhMFnTg)',
+           'Электронный термометр': '[Для просмотра щелкните на картинку](https://youtu.be/iA6yIxAYawE)'}
