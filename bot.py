f7d9e296ff628b4b0c490ca95dedb30718a01849
stufforama <stufforama.arh@gmail.com>
2018-03-29 17:43:49 +0300

    merge

--- bot.py
+++ bot.py
@@ -15,6 +15,48 @@ import botan
 from lxml.html import fromstring
 from lxml import cssselect
 from datetime import datetime
+import urllib
+from io import StringIO
+
+print('updating certificates')
+
+url_template = 'http://and-rus.ru/service/{}_{}/'
+urls = []
+for author in ['calibrpressuread', 'termometrcalibration']:
+    for page in range(1,200):
+        urls.append(url_template.format(author, page))
+        
+# flatten = lambda l: [item for sublist in l for item in sublist]
+
+
+# In[ ]:
+
+text = ''
+for url in urls:
+    try:
+        html = get(url).text
+        dom = fromstring(html)
+        dom.make_links_absolute(url)
+        css_elements = dom.cssselect('.menu-left')
+        text += "\n".join([css_elements[t].text_content().strip() +';' + css_elements[t].get('href').strip() for t in range(3,len(css_elements))])
+        if css_elements != []:
+            text += '\n'
+    except AttributeError:
+        pass
+
+data = pd.read_csv(StringIO(text), sep=";",header=None, encoding = 'utf8')
+
+data.columns = ['text', 'url']
+pattern = '-[0-9]{3,4}'
+data['sku'] = data['text'].apply(lambda x: re.findall(string=x, pattern = pattern)[0].rsplit('-')[1])
+data.loc[data['sku']=='911', 'sku'] = data.loc[data['sku']=='911']['text'].apply(lambda x: '911c' if len(re.findall(string=x, pattern='[cC]+'))>0 else '911')
+data['start'] = data['text'].apply(lambda x: re.findall(string=x, pattern = '[0-9]{6,}')[0])
+data['end'] = data['text'].apply(lambda x: re.findall(string=x, pattern = '[0-9]{6,}')[1])
+data['start'] = pd.to_numeric(data['start'])
+data['end'] = pd.to_numeric(data['end'])
+data['url'] = data['url'].apply(lambda x: 'http://' + urllib.parse.quote(x.rsplit('//')[1]))
+data.drop(['text'], axis=1, inplace = True)
+
 
 
 
@@ -25,8 +67,8 @@ API_KEY = os.environ.get('API_KEY')
 BOTAN_KEY = os.environ.get('BOTAN_KEY')
 
 #информация о свидетельствах
-print('loadind certificates')
-data = pd.read_csv('doclist.csv', delimiter= ";", encoding = 'utf8')
+# print('loadind certificates')
+# data = pd.read_csv('doclist.csv', delimiter= ";", encoding = 'utf8')
 
 #информация о сервисных центрах
 print('loadind service centeres')
